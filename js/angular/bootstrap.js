37246bd33e4afbe207af41df22ef9df9a266c115
Elliot Hesp <elliot.hesp@gmail.com>
2014-06-24 09:28:55 +0100

    Update

--- js/angular/bootstrap.js
+++ js/angular/bootstrap.js
@@ -1,45 +1,48 @@
 var app = angular.module('SSMS', ['ui.bootstrap', 'ngCookies']);
 
-app.controller('MasterCtrl', function($scope, $cookies) {
-
-	var mobileView = 992;
-
-	// Get current window size
-	$scope.getWidth = function() { return window.innerWidth; };
-
-	if(angular.isDefined($cookies.toggle)) {
-
-		$scope.toggle = $cookies.toggle;
-
-	} else {
-
-		$scope.$watch($scope.getWidth, function(newValue, oldValue) {
-
-
-			if(newValue < mobileView) {
-
-				$scope.toggle = false;
-
-			}
-			else {
-
-				$scope.toggle = true;
-
-			}
-
-		});
-	}
-
-	$scope.toggleSidebar = function() {
-
-		$scope.toggle = ! $scope.toggle;
-
-		$cookies.toggle = $scope.toggle;
-
-
-	};
-
+app.controller('MasterCtrl', function($scope, $rootScope, $cookieStore) {
+ 
+        var mobileView = 992;
+ 
+        // Get current window size
+        $scope.getWidth = function() { return window.innerWidth; };
+ 
+        if(angular.isDefined($cookieStore.get('toggle'))) {
+ 
+                $rootScope.toggle = $cookieStore.get('toggle');
+ 
+ 
+        } else {
+ 
+                $scope.$watch($scope.getWidth, function(newValue, oldValue) {
+ 
+ 
+                        if(newValue < mobileView) {
+ 
+                                $rootScope.toggle = false;
+ 
+ 
+                        }
+                        else {
+ 
+                                $rootScope.toggle = true;
+ 
+ 
+                        }
+ 
+                });
+        }
+ 
+        $scope.toggleSidebar = function() {
+ 
+                $rootScope.toggle = ! $scope.toggle;
+ 
+                $cookieStore.put('toggle', $rootScope.toggle);
+ 
+ 
+        };
+ 
      window.onresize = function() { $scope.$apply(); };
-  
-
+ 
+ 
 });

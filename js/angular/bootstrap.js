03bc6cfa5cebbf36f4a9cf5e4fdffe4626c6f945
Elliot Hesp <elliot.hesp@gmail.com>
2014-07-31 18:37:16 +0100

    Added loading directive

--- js/angular/bootstrap.js
+++ js/angular/bootstrap.js
@@ -1,5 +1,17 @@
 var app = angular.module('Dashboard', ['ui.bootstrap', 'ngCookies']);
 
+/**
+ * Loading Directive
+ * @see http://tobiasahlin.com/spinkit/
+ */
+app.directive('loading', function () {
+    return {
+        restrict: 'AE',
+        replace: 'false',
+        template: '<div class="loading"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>'
+    }
+});
+
 app.controller('MasterCtrl', function($scope, $cookieStore) {
 
     /**

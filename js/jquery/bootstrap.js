6c7d63f3d045fbfd55d626ebbb0596eb64c72e89
Elliot Hesp <elliot.hesp@gmail.com>
2014-08-06 16:32:53 +0100

    Updated jquery bootstrap file

--- js/jquery/bootstrap.js
+++ js/jquery/bootstrap.js
@@ -1,39 +1,51 @@
-$( document ).ready(function() {
+ /**
+ * Sidebar Toggle & Cookie Control
+ *
+ */
+var mobileView = 992;
 
-    var mobileView = 992;
+function getWidth() { return window.innerWidth; }
 
-    function getWidth() { return window.innerWidth; }
-
-    $( window ).resize(function()
+function detect()
+{
+    if (getWidth() >= mobileView)
     {
-        if (getWidth() >= mobileView)
+        if ($.cookie('toggle') === undefined)
         {
-            if ($.cookie('toggle') === undefined)
+            console.log("undefined");
+            $("#page-wrapper").addClass("active");
+        }
+        else
+        {
+            if($.cookie('toggle') == 'true')
             {
-                console.log("undefined");
+                console.log("true");
                 $("#page-wrapper").addClass("active");
             }
             else
             {
-                console.log($.cookie('toggle'));
-                if(! $.cookie('toggle'))
-                {
-                    
-                    $("#page-wrapper").removeClass("active");
-                }
-                else
-                {
-                    console.log("here21234");
-                    $("#page-wrapper").addClass("active");
-                }
+                console.log("false");
+                $("#page-wrapper").removeClass("active");
             }
         }
-        else
-        {
-            $("#page-wrapper").removeClass("active");
-        }
+    }
+    else
+    {
+        $("#page-wrapper").removeClass("active");
+    }
+}
+
+detect();
+
+/* On Document Ready */
+$(function(){
+
+    // Detect any resizes and run our detect() function
+    $( window ).resize(function() {
+        detect();
     });
 
+    // Sidebar toggle & cookie toggle
     $("#toggle").click(function()
     {
         $("#page-wrapper").toggleClass("active");
@@ -41,6 +53,8 @@ $( document ).ready(function() {
         $.cookie('toggle', $("#page-wrapper").hasClass("active"));
     });
 
-    $(window).trigger('resize');
+    $('.dropdown-toggle').dropdown()
 
+    // Set a tooltip on any elements with rel="tooltip" - see example
+    $('[rel="tooltip"]').tooltip();
 });

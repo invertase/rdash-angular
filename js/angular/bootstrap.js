883150eb2ae4eab3d633431512c5306f686b872b
Mathew Goldsborough <mathew.goldsborough@foreveroceans.com>
2014-08-11 05:49:49 -0400

    Updated UI Bootstrap and created stub angular controller for Alerts.

--- js/angular/bootstrap.js
+++ js/angular/bootstrap.js
@@ -58,3 +58,19 @@ app.controller('MasterCtrl', function($scope, $cookieStore) {
 
     window.onresize = function() { $scope.$apply(); };
 });
+
+// The alerts controller.
+app.controller('AlertsCtrl', function($scope) {
+    $scope.alerts = [
+        { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+        { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+    ];
+
+    $scope.addAlert = function() {
+        $scope.alerts.push({msg: 'Another alert!'});
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+});

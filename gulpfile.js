ded84e14f465209ec131536d0daa52d68560337d
Mathew Goldsborough <mathew.goldsborough@foreveroceans.com>
2014-09-01 10:17:26 -0400

    Gulp optimizations to combine and minify js/css files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,117 @@
 var gulp    = require('gulp'),
   less      = require('gulp-less'),
-  usemin    = require('gulp-usemin'),
   wrap      = require('gulp-wrap'),
+  watch     = require('gulp-watch'),  
+  concat    = require('gulp-concat'),
+  uglify    = require('gulp-uglify'), 
   connect   = require('gulp-connect'),
-  watch     = require('gulp-watch');
+  cssmin    = require('gulp-minify-css'),
+  templates = require('gulp-angular-templates');
+
+var vendors = [
+  'src/bower_components/angular/angular.js',
+  'src/bower_components/angular-cookies/angular-cookies.js',
+  'src/bower_components/angular-bootstrap/ui-bootstrap.js',
+  'src/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+  'src/bower_components/angular-ui-router/release/angular-ui-router.js',
+];
+
+var styles = [
+  'src/bower_components/bootstrap/dist/css/bootstrap.css',
+  'src/bower_components/font-awesome/css/font-awesome.css',
+  'src/less/dashboard/variables.less',
+  'src/less/dashboard/mixins.less',
+  'src/less/dashboard/main.less',
+  'src/less/dashboard/loading.less',
+  'src/less/dashboard/content.less',
+  'src/less/dashboard/header.less',
+  'src/less/dashboard/sidebar.less',
+  'src/less/dashboard/widgets.less',
+  'src/less/dashboard/hamburg.less',
+];
+
+var fonts = [
+  'src/fonts/**.*',
+  'src/bower_components/bootstrap/dist/fonts/*.{ttf,woff,eof,svg}',
+  'src/bower_components/font-awesome/fonts/*.{ttf,woff,eof,svg}'
+];
 
 var paths = {
-  js: 'src/js/**/*.*',
-  fonts: 'src/fonts/**.*',
+  js: ['src/js/**/*.*', 'dist/js/templates.js'],
+  files: ['src/index.html'],
   images: 'src/img/**/*.*',
-  styles: 'src/less/**/*.less',
-  index: 'src/index.html',
-  bower_fonts: 'src/bower_components/**/*.{ttf,woff,eof,svg}',
-  bower_components: 'src/bower_components/**/*.*',
+  templates: 'src/templates/**/*.html',
+  fonts: fonts,
+  styles: styles,
+  vendors: vendors
 };
 
+// The name of the Angular module which will be injected into the templates.
+var moduleName = 'Dashboard';
 
-gulp.task('usemin', function() {
-  return gulp.src(paths.index)
-    .pipe(usemin({
-      less: ['concat', less()],
-      js: ['concat', wrap('(function(){ \n<%= contents %>\n})();')],
-    }))
-    .pipe(gulp.dest('dist/'));
+// Minify and copy all 3rd party libs to vendors.min.js 
+gulp.task('copy-vendors', function() {
+  return gulp.src(paths.vendors)
+    .pipe(uglify())
+    .pipe(concat('vendors.min.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
-/**
- * Copy assets
- */
-gulp.task('copy-assets', ['copy-images', 'copy-fonts', 'copy-bower_fonts']);
+// Minify and copy all dashboard script files to dashboard.min.js
+gulp.task('copy-scripts', function() {
+  return gulp.src(paths.js)
+    .pipe(uglify())
+    .pipe(concat('dashboard.min.js'))
+    .pipe(gulp.dest('dist/js'));
+});
 
+// Minify and copy all angular templates to templates.min.js
+gulp.task('copy-templates', function() {
+  return gulp.src(paths.templates)
+    .pipe(templates({module: moduleName}))
+    .pipe(uglify())
+    .pipe(concat('templates.min.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+// Copy all static/HTML files to output directory
+gulp.task('copy-files', function(){
+  return gulp.src(paths.files)
+    .pipe(gulp.dest('dist'));
+});
+
+// Copy all images to output directory
 gulp.task('copy-images', function(){
   return gulp.src(paths.images)
     .pipe(gulp.dest('dist/img'));
 });
 
+// Copy all fonts to output directory
 gulp.task('copy-fonts', function(){
   return gulp.src(paths.fonts)
     .pipe(gulp.dest('dist/fonts'));
 });
 
-gulp.task('copy-bower_fonts', function(){
-  return gulp.src(paths.bower_fonts)
-    .pipe(gulp.dest('dist/lib'));
+// Compile less styles into dashboard.css
+gulp.task('compile-less', function(){
+  return gulp.src(paths.styles)
+      .pipe(less())
+      .pipe(cssmin())
+      .pipe(concat('dashboard.min.css'))
+      .pipe(gulp.dest('dist/css'));
 });
 
 /**
  * Watch src
  */
 gulp.task('watch', function () {
-  gulp.watch([paths.styles, paths.index, paths.js], ['usemin']);
-  gulp.watch([paths.images], ['copy-images']);
-  gulp.watch([paths.fonts], ['copy-fonts']);
-  gulp.watch([paths.bower_fonts], ['copy-bower_fonts']);
+  gulp.watch(paths.vendors, ['copy-vendors']);
+  gulp.watch(paths.js, ['copy-scripts']);
+  gulp.watch(paths.templates, ['copy-templates']);
+  gulp.watch(paths.files, ['copy-files']);
+  gulp.watch(paths.images, ['copy-images']);
+  gulp.watch(paths.fonts, ['copy-fonts']);
+  gulp.watch(paths.styles, ['compile-less']);
 });
 
 gulp.task('webserver', function() {
@@ -68,14 +127,5 @@ gulp.task('livereload', function() {
     .pipe(connect.reload());
 });
 
-/**
- * Compile less
- */
-gulp.task('compile-less', function(){
-  return gulp.src(paths.styles)
-      .pipe(less())
-      .pipe(gulp.dest('dist/css'));
-});
-
-gulp.task('build', ['usemin', 'copy-assets']);
+gulp.task('build', ['copy-vendors', 'copy-scripts', 'copy-templates', 'copy-files', 'copy-images', 'copy-fonts', 'compile-less']);
 gulp.task('default', ['build', 'webserver', 'livereload', 'watch']);

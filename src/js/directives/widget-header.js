b4ae4fcb5e2880e6594be63dc72bbab27c4f81aa
Huu Tho Nguyen <huu.thonguyen76@gmail.com>
2015-03-16 22:22:39 +0700

    Add grid col for search bar more beauty

--- src/js/directives/widget-header.js
+++ src/js/directives/widget-header.js
@@ -14,7 +14,7 @@ function rdWidgetTitle() {
             icon: '@'
         },
         transclude: true,
-        template: '<div class="widget-header"><div class="row"><div class="pull-left"><i class="fa" ng-class="icon"></i> {{title}} </div><div class="pull-right" ng-transclude></div></div></div>',
+        template: '<div class="widget-header"><div class="row"><div class="pull-left"><i class="fa" ng-class="icon"></i> {{title}} </div><div class="pull-right col-xs-6 col-sm-4" ng-transclude></div></div></div>',
         restrict: 'E'
     };
     return directive;
